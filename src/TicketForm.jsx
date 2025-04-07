0a59ca2352ebbc26af4110b08c6d0892350ac91d
Linh Le <lethimylinh.2001@gmail.com>
2025-04-06 23:25:32 -0300

    trying to fix something wrong

--- src/TicketForm.jsx
+++ src/TicketForm.jsx
@@ -2,53 +2,55 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import  './App.css';
+import './App.css';
 
 const TicketForm = () => {
-    const { register, handleSubmit, formState: { errors }, reset } = useForm();
-  
-    const onSubmit = async (data) => {
-      try {
-        // Convert concertId and quantity to numbers before sending
-        const formattedData = {
-          ...data,
-          concertId: Number(data.concertId),
-          quantity: Number(data.quantity)
-        };
-  
-        // Making the POST request to the API
-        const response = await axios.post(
-          `https://api.allorigins.win/get?url=${import.meta.env.VITE_API_URL}/api/ticketHub`, 
-          formattedData, 
-          {
-            headers: {
-              'Content-Type': 'application/json', 
-            },
-          }
-        );
-  
-        if (response.status === 200) {
-          console.log("API Response:", response.data); 
-  
-          // Display success message
-          toast.success('Purchase successful!');
-          reset();
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
-        } else {
-          toast.error('Failed to process the purchase. Please try again.');
+  const onSubmit = async (data) => {
+    try {
+      // Convert concertId and quantity to numbers before sending
+      const formattedData = {
+        ...data,
+        concertId: Number(data.concertId),
+        quantity: Number(data.quantity),
+      };
+
+      // Make sure to log the formattedData for debugging
+      console.log('Formatted Data:', formattedData);
+
+      // Making the POST request to the API (using a CORS proxy)
+      const response = await axios.post(
+        `https://api.allorigins.win/get?url=${import.meta.env.VITE_API_URL}/api/ticketHub`, 
+        formattedData, 
+        {
+          headers: {
+            'Content-Type': 'application/json', 
+          },
         }
-      } catch (error) {
-        console.error("Error occurred during API call:", error);
-        toast.error('There was an error submitting the form.');
+      );
+
+      if (response.status === 200) {
+        console.log("API Response:", response.data);
+
+        // Display success message
+        toast.success('Purchase successful!');
+        reset();
+      } else {
+        toast.error('Failed to process the purchase. Please try again.');
       }
-    };
+    } catch (error) {
+      console.error("Error occurred during API call:", error);
+      toast.error('There was an error submitting the form.');
+    }
+  };
 
   return (
     <div className="container-fluid h-100">
       <div className="row h-100">
-        <div className="col-md-5 d-flex align-items-center bg-black justify-content-center text-white p-4" >
+        <div className="col-md-5 d-flex align-items-center bg-black justify-content-center text-white p-4">
           <div className="form-container w-100">
-            <h1 className="display-4 fw-semibold" style={{color: '#f34a8e' }}>World Tour Ticket Form</h1>
+            <h1 className="display-4 fw-semibold" style={{ color: '#f34a8e' }}>World Tour Ticket Form</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="row">
                 {/* First Row: Form */}
@@ -71,7 +73,7 @@ const TicketForm = () => {
                       name="email"
                       {...register('email', { 
                         required: 'Email is required', 
-                        pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'Invalid email format' } 
+                        pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'Invalid email format' }
                       })}
                     />
                     {errors.email && <span>{errors.email.message}</span>}
@@ -94,7 +96,7 @@ const TicketForm = () => {
                       name="phone"
                       {...register('phone', { 
                         required: 'Phone is required', 
-                        pattern: { value: /^\d{10}$/, message: 'Phone number must be 10 digits' } 
+                        pattern: { value: /^\d{10}$/, message: 'Phone number must be 10 digits' }
                       })}
                     />
                     {errors.phone && <span>{errors.phone.message}</span>}
@@ -110,7 +112,7 @@ const TicketForm = () => {
                       name="quantity"
                       {...register('quantity', { 
                         required: 'Quantity is required', 
-                        min: { value: 1, message: 'Quantity must be at least 1' } 
+                        min: { value: 1, message: 'Quantity must be at least 1' }
                       })}
                     />
                     {errors.quantity && <span>{errors.quantity.message}</span>}
@@ -123,7 +125,7 @@ const TicketForm = () => {
                       name="creditCard"
                       {...register('creditCard', { 
                         required: 'Credit card number is required', 
-                        pattern: { value: /^\d{16}$/, message: 'Credit card number must be 16 digits' } 
+                        pattern: { value: /^\d{16}$/, message: 'Credit card number must be 16 digits' }
                       })}
                     />
                     {errors.creditCard && <span>{errors.creditCard.message}</span>}
@@ -136,7 +138,7 @@ const TicketForm = () => {
                       name="expiration"
                       {...register('expiration', { 
                         required: 'Expiration date is required', 
-                        pattern: { value: /^(0[1-9]|1[0-2])\/\d{2}$/, message: 'Invalid expiration date format. Use MM/YY' } 
+                        pattern: { value: /^(0[1-9]|1[0-2])\/\d{2}$/, message: 'Invalid expiration date format. Use MM/YY' }
                       })}
                     />
                     {errors.expiration && <span>{errors.expiration.message}</span>}
@@ -149,7 +151,7 @@ const TicketForm = () => {
                       name="securityCode"
                       {...register('securityCode', { 
                         required: 'Security code is required', 
-                        pattern: { value: /^\d{3}$/, message: 'Security code must be 3 digits' } 
+                        pattern: { value: /^\d{3}$/, message: 'Security code must be 3 digits' }
                       })}
                     />
                     {errors.securityCode && <span>{errors.securityCode.message}</span>}
@@ -192,7 +194,7 @@ const TicketForm = () => {
                       name="postalCode"
                       {...register('postalCode', { 
                         required: 'Postal code is required', 
-                        pattern: { value: /(^\d{5}(-\d{4})?$)|(^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$)/, message: 'Invalid postal code format' } 
+                        pattern: { value: /(^\d{5}(-\d{4})?$)|(^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$)/, message: 'Invalid postal code format' }
                       })}
                     />
                     {errors.postalCode && <span>{errors.postalCode.message}</span>}
@@ -212,15 +214,14 @@ const TicketForm = () => {
 
               <button type="submit">Submit</button>
             </form>
-              </div>
-              </div>
-        {/* Right Column: Image */}
-        <div className="col-md-7 bg-cover d-none d-md-block" style={{ backgroundImage: "url('https://res.klook.com/image/upload/v1673236312/v2rfb2znqw8ktmprsczd.jpg')" }}>
+          </div>
         </div>
+
+        {/* Right Column: Image */}
+        <div className="col-md-7 bg-cover d-none d-md-block" style={{ backgroundImage: "url('https://res.klook.com/image/upload/v1673236312/v2rfb2znqw8ktmprsczd.jpg')" }}></div>
       </div>
     </div>
-   
   );
-}
+};
 
 export default TicketForm;
